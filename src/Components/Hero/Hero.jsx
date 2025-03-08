de4a2eb1f091d5339aaf91e21a890a576f0b7ff0
unknown <samehrizk1993@gmail.com>
2025-03-08 17:01:33 +0200

    adding my project

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      {/* <div className="hero-text">
+        <h1>We insure better education for a better world</h1>
+      </div> */}
+    </div>
+  );
+}
+
+export default Hero;
