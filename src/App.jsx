1f6629ad908f740b20bfef52e42efec29e9f3749
unknown <samehrizk1993@gmail.com>
2025-03-09 14:48:57 +0200

    adding programs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/Programs/Programs";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
   );
 }
